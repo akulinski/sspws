958763159d7fb25ec61064b0f8b1024cd7fd089c
Albert Kuliński <albert.kulinski@hotmail.com>
2018-12-20 13:36:53 +0100

    initial commit

---
+++ src/main/java/com/akulinski/sspws/SspwsApplication.java
@@ -0,0 +1,14 @@
+package com.akulinski.sspws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SspwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SspwsApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/akulinski/sspws/SspwsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.akulinski.sspws;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SspwsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
